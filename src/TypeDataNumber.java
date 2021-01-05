dd0642b46bb79b016d94a2cfc885b6d2e8558fda
Ridwan226 <4515210063@univpancasila.ac.id>
2021-01-05 23:07:05 +0700

    Type Data dan Konversi

--- src/TypeDataNumber.java
+++ src/TypeDataNumber.java
@@ -7,6 +7,25 @@ public class TypeDataNumber {
       long iniLong = 12345678934234L;  // -9223372036854775808   sampai 9223372036854775807
 
 
+        int decimalInt = 34;
+        int hexDecimal = 0x23FF;
+        int binarDecimal = 0b10100011;
+
+
+        // Literasi Under Scor
+
+        int amount = 1_000_000;
+
+        // Widening Casting = byte -> short -> int -> long -> float -> doubel
+
+        byte iniBytes = 10;
+        short iniShorts = iniBytes;
+
+        // nowring Casting Kebalikan nya dari Widenning Casting
+
+        float iniFloat2 = (float) iniInteger;
+
+
       System.out.println(iniShort);
     }
 }
