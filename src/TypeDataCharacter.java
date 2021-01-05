6c36b709586ed9a2dd31d4f206f281ecab1575f1
Ridwan226 <4515210063@univpancasila.ac.id>
2021-01-05 23:10:57 +0700

    Type Data Caracter

---
+++ src/TypeDataCharacter.java
@@ -0,0 +1,13 @@
+public class TypeDataCharacter {
+    public static void main(String[] args) {
+
+        char r = 'R';
+        char i = 'I';
+        char d = 'D';
+
+        System.out.println(r);
+        System.out.println(i);
+        System.out.println(d);
+
+    }
+}
