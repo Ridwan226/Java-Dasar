7ab7d050aa2197a7440cbee0c49a3f1bc387b151
Ridwan226 <4515210063@univpancasila.ac.id>
2021-01-05 22:53:04 +0700

    first commit

---
+++ src/TypeDataNumber.java
@@ -0,0 +1,12 @@
+public class TypeDataNumber {
+    public static void main(String[] args) {
+
+      byte iniByte = 127;  // -128 Sampai  127
+      short iniShort = 1000; // -32768 sampai 32767
+      int iniInteger = 32334; // -2147483648 sampai 2147483647
+      long iniLong = 12345678934234L;  // -9223372036854775808   sampai 9223372036854775807
+
+
+      System.out.println(iniShort);
+    }
+}
