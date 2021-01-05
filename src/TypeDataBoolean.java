0d547945139be3bce6290971c24bfc218dc000ef
Ridwan226 <4515210063@univpancasila.ac.id>
2021-01-05 23:17:43 +0700

    Type Data Boolean

---
+++ src/TypeDataBoolean.java
@@ -0,0 +1,11 @@
+public class TypeDataBoolean {
+    public static void main(String[] args) {
+
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println(benar);
+        System.out.println(salah);
+
+    }
+}
